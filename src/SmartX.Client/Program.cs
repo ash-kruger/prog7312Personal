a3dbba6aa0e6279c108f522e17d6079020b964a8
A Kruger <ash.kruger0@gmail.com>
2026-09-14 14:25:37 +0200

    fix: retarget to net9.0 and resolve Razor/Program.cs build errors

--- src/SmartX.Client/Program.cs
+++ src/SmartX.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SmartX.Client;
 using SmartX.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
